94ea7de29ff3a88bc2c61b30bc1c088539f1e2df
Olesya090585 <okochman@ya.ru>
2018-11-09 08:47:39 +0300

    Homework6

---
+++ homework6_Varnakova/2/index.php
@@ -0,0 +1,13 @@
+<?php
+include "config.php";
+?>
+
+<form action="" method="post">
+    <input type="number" name="a" required>
+    <input type="submit" name="operation" value="+">
+    <input type="submit" name="operation" value="-">
+    <input type="submit" name="operation" value="*">
+    <input type="submit" name="operation" value="/">
+    <input type="number" name="b" required>
+</form>
+
